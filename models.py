39af064767d01185fba6535d8481ed2775736b29
陳瑞賢 <skillchen skillchen@gmail.com>
2017-11-03 14:44:39 +0800

    first commit

---
+++ models.py
@@ -0,0 +1,10 @@
+from django.db.models import (
+    Model,
+    TextField
+)
+
+# Create your models here.
+class JobListing(Model):
+    title = TextField(null=True,blank=True)
+    description = TextField(null=True, blank=True)
+
---
+++ static/jobs_app/assets/js/our_custom_script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by skillchen on 2017/11/3.
+ */
+
+console.log("Hey,this is our javascript first code.");
+
+function showPostForm() {
+   form = $('#post_form');
+   form.css("opacity","1");
+   form.css("height","190px");
+}
